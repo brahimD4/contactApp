e25e794286a5367faf6d2625c41929a81043f880
brahimD4 <foukar.brahim@gmail.com>
2024-03-29 15:31:32 +0000

    ajouter un fichier json et modifier quelque dans fichier css

--- js/main.js
+++ js/main.js
@@ -1,45 +1,11 @@
 
 
+
 // async function get(){
-//   const response=  fetch("https://randomuser.me/api/?results=5000");
-//  const data=await response.JSON();
-
-//   // const obj = JSON.parse(data);
-//    console.log(obj);
-
+//   const response= await fetch("../Json/resultats.json");
+//   const data = await response.json();
+//   console.log(data);
+//    return data;
 // }
 
-//  get();
-
-
-// fetch("https://randomuser.me/api/")
-// .then(function(response){
-  
-//   console.log(response);
-//   return response.json();
-// }).then(function(data){
-//   console.log(data); 
-// });
-//  async function getDataFromAPI(url) {
-//   return fetch(url)
-//       .then(response => {
-//           if (!response.ok) {
-//               throw new Error('Erreur HTTP ' + response.status);
-//           }
-//           return response.json();
-//       })
-//       .catch(error => {
-//           console.error('Erreur lors de la récupération des données depuis l\'API:', error);
-//       });
-// }
-// // var obj=getDataFromAPI("https://randomuser.me/api/");
-// getDataFromAPI("https://randomuser.me/api/")
-//     .then(data => {
-//         // Traiter les données et les transformer en objets
-//         const objects = data.map(item => {
-//             return {
-//                 attribute1: item.attribute1,
-//                 attribute2: item.attribute2
-//             };
-//         });
-   
+// get();
